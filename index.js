717afa594acf0682b1f2b80634801ab678ec1a4c
UA CR <blazer5@email.arizona.edu>
2021-07-06 11:43:26 -0700

    add style to cards

--- index.js
+++ index.js
@@ -23,52 +23,52 @@ const questions = [
         name: 'officeNumber',
         message: "What is your office number?"
     },
-    {
-        type: 'list',
-        name: 'addTeamMember',
-        message: "Would you like to add a team member?",
-        choices: ['Engineer', 'Intern', 'Finished building team'] 
-    },
-    {
-        type: 'input',
-        name: 'engineerName',
-        message: "Name of engineer:"
-    },
-    {
-        type: 'input',
-        name: 'engineerId',
-        message: "Employee ID of this team member?"
-    },
-    {
-        type: 'input',
-        name: 'engineerEmail',
-        message: "Email address of this team member?"
-    },
-    {
-        type: 'input',
-        name: 'engineerGit',
-        message: "GitHub username of this team member?"
-    },
-    {
-        type: 'input',
-        name: 'internName',
-        message: "Name of Intern:"
-    },
-    {
-        type: 'input',
-        name: 'internId',
-        message: "Employee ID of this team member?"
-    },
-    {
-        type: 'input',
-        name: 'internEmail',
-        message: "Email address of this team member?"
-    },
-    {
-        type: 'input',
-        name: 'internGit',
-        message: "GitHub username of this team member?"
-    },
+    // {
+    //     type: 'list',
+    //     name: 'addTeamMember',
+    //     message: "Would you like to add a team member?",
+    //     choices: ['Engineer', 'Intern', 'Finished building team'] 
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'engineerName',
+    //     message: "Name of engineer:"
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'engineerId',
+    //     message: "Employee ID of this team member?"
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'engineerEmail',
+    //     message: "Email address of this team member?"
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'engineerGit',
+    //     message: "GitHub username of this team member?"
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'internName',
+    //     message: "Name of Intern:"
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'internId',
+    //     message: "Employee ID of this team member?"
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'internEmail',
+    //     message: "Email address of this team member?"
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'internGit',
+    //     message: "GitHub username of this team member?"
+    // },
 ];
 
 const promptUser = () => {
@@ -76,7 +76,7 @@ const promptUser = () => {
         .then(data => {
             const pageHTML = generateMarkdown(data);
 
-            fs.writeFile('index.html', pageHTML, err => {
+            fs.writeFile('./dist/index.html', pageHTML, err => {
                 if (err) throw err;
                 console.log('Team Portfolio complete! Check out index.html to see the output!');
 
